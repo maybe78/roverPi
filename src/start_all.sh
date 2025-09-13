0b6294ff26cc40388cd5947f81423555541fe5c3
maybe78 <maybe78@gmail.com>
2025-09-13 03:20:25 +0300

    Camera device added to service for proper start

--- src/start_all.sh
+++ src/start_all.sh
@@ -4,6 +4,11 @@ MJPEG_PLUGIN_DIR=/home/volodya/mjpg-streamer/mjpg-streamer-experimental/_build/p
 cd $PROJECT_DIR/src/
 
 # Start Cam Stream
+while [ ! -e /dev/video0 ]; do
+  echo "Waiting for camera (/dev/video0)..."
+  sleep 1 
+done
+echo "Cam was found!"
 mjpg_streamer -i "$MJPEG_PLUGIN_DIR/input_uvc/input_uvc.so -d /dev/video0 -r 854x480 -f 10" -o "$MJPEG_PLUGIN_DIR/output_http/output_http.so -p 8080" &
 # Start main python app
 $PROJECT_DIR/venv/bin/python $PROJECT_DIR/src/main.py
